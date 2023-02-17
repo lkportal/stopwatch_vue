a4b1879f96bd053daaea66734957f6428d1325af
lkportal <lksxim@gmail.com>
2023-02-17 00:15:15 -0300

    script+

--- script/script.js
+++ script/script.js
@@ -1,28 +1,44 @@
 const vm = new Vue({
     el:'#app',
     data:{
-        segundo:0,
+        segundo: 0,
+        segundoParte2:0,
         minuto:0,
+        minutoParte2:0,
         horas:0,
+        horasParte2:0,
         tempo:3
     },
     methods:{
         time(){
-           
-            setInterval(()=>{
-                if(this.tempo > this.segundo ){
-                    this.segundo += 1
+          const timeSeg = setInterval(()=>{
+            if(this.tempo > this.segundo){
+                this.segundo++
+                if(this.segundo == 3){
+                    this.segundo =0
+                    this.segundoParte2++
                 }
-                 if(this.segundo == 3 ){
-                    this.segundo = 0
-                    this.minuto += 1
-                } if(this.minuto == 3){
+            }
+            if(this.segundoParte2 > 3){ 
+                this.segundoParte2=0
+                 this.minuto += 1
+                 if(this.minuto== 3){
                     this.minuto = 0
-                    this.horas += 1
+                    this.minutoParte2 += 1
+                 }
+            }if(this.minutoParte2 == 3){
+                this.minutoParte2 = 0
+                this.horas += 1
+                if(this.horas == 3){
+                    horas=0
+                    this.horasParte2 +=1
+                    
+                }if(this.horasParte2 == 3){
+                    this.horasParte2 = 0
                 }
+            }
             },1000)
-           
-        },
+        }
       
         
     }
